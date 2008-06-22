b1e9a9a0255b9f514d1c198e37b7774f867dfd36
mkjems <mkjems@c028e98f-4550-0410-83cb-ff94a89eda61>
2008-06-22 19:16:07 +0000

    start

---
+++ trunk/system/i18n/pl_PL/cache.php
@@ -0,0 +1,10 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+$lang = array
+(
+	'driver_not_supported' => 'Sterownik %s pamięci podręcznej nie istnieje',
+	'extension_not_loaded' => 'Aby użyć tego sterownika musi zostać załadowane rozszerzenie PHP %s.',
+	'unwritable'           => 'Wybrane w konfiguracji miejsce, %s, jest tylko do odczytu.',
+	'resources'            => 'Zachowanie danych w pamięci podręcznej jest niemożliwe z powodu braku możliwości serializacji.',
+	'driver_error'         => '%s',
+);
---
+++ trunk/system/i18n/pl_PL/encrypt.php
@@ -0,0 +1,7 @@
+<?php defined('SYSPATH') or die('No direct access allowed.');
+
+$lang = array
+(
+	'requires_mcrypt'   => 'Aby użyć biblioteki szyfrowania, należy włączyć mcrypt w konfiguracji PHP.',
+	'no_encryption_key' => 'Aby użyć biblioteki szyfrowania, należy zdefiniować klucz szyfrujący w pliku konfiguracji.',
+);
