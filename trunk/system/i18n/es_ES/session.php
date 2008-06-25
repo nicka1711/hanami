bc38f2ed1a3d95a5768332e107d8ca3ba5429f3d
dpommer <dpommer@c028e98f-4550-0410-83cb-ff94a89eda61>
2008-06-25 12:04:43 +0000

    Setting svn:externals to system http://svn.kohanaphp.com/tags/2.1.3/system

--- trunk/system/i18n/es_ES/session.php
+++
@@ -1,8 +0,0 @@
-<?php defined('SYSPATH') or die('No direct script access.'); 
-
-$lang = array
-(
-	'driver_not_supported' => 'No se encuentra el driver, %s, requerido por la librería Session.',
-	'driver_implements'    => 'El driver requerido por la librería Session, %s, no implementa la clase Session_Driver.',
-	'invalid_session_name' => 'El parametro session_name, %s, no es valido. Solo debe contener caracteres alfanuméricos y al menos uno debe de ser una letra.',
-);
--- trunk/system/i18n/pl_PL/encrypt.php
+++
@@ -1,7 +0,0 @@
-<?php defined('SYSPATH') or die('No direct access allowed.');
-
-$lang = array
-(
-	'requires_mcrypt'   => 'Aby użyć biblioteki szyfrowania, należy włączyć mcrypt w konfiguracji PHP.',
-	'no_encryption_key' => 'Aby użyć biblioteki szyfrowania, należy zdefiniować klucz szyfrujący w pliku konfiguracji.',
-);
