83cc6105deae0f056120afb36f85e86e008ab3ef
dpommer <dpommer@c028e98f-4550-0410-83cb-ff94a89eda61>
2008-07-25 12:44:25 +0000

    Kohana API changes and other improvements

--- installation/controllers/installation.php
+++ installation/controllers/installation.php
@@ -6,13 +6,18 @@
  */
 
 
-class Installation_Controller extends Hanami_Frontend_Controller {
+class Installation_Controller extends Template_Controller {
+
+	// Template view name
+	public $template = 'install';
+
+    protected $page;
 
 	protected $application;
-	
-	protected $lang;
 
-	public $page_id = 'installation';
+//	protected $lang;
+
+//	public $page_id = 'installation';
 
 	public function __construct()
 	{
@@ -21,7 +26,7 @@ class Installation_Controller extends Hanami_Frontend_Controller {
 		/**
 		 * TODO: nessecary each request?
 		 */
-		$avail_langs = array('de_DE');//Config::item('locale.available_langs');
+		$avail_langs = array('de_DE');//Kohana::config('locale.available_langs');
 		$langs = array();
 		foreach($avail_langs as $lang => $name)
 		{
@@ -29,12 +34,13 @@ class Installation_Controller extends Hanami_Frontend_Controller {
 		}
 		$this->lang = current($langs);
 
-		Config::set('locale.language', $this->lang);
+		Kohana::config('locale.language', $this->lang);
 
 
 		$this->application = new Installation;
 
-		$this->page_title[] = Kohana::lang('install.installation');
+		$this->page        = new Page;
+		$this->page->title[] = Kohana::lang('install.installation');
 	}
 
 	/**
@@ -42,14 +48,13 @@ class Installation_Controller extends Hanami_Frontend_Controller {
 	 */
 	public function index()
 	{
-		echo Kohana::debug($_POST);
 		// Redirect to the next step "license", if a language is choosen
 		(isset($_POST['lang'][0])) and url::redirect('installation/license');
 
 		// Filling the template
 		$this->page_title[] = Kohana::lang('install.welcome');
 		$this->template->content = View::factory('installation/welcome')
-			->set('avail_langs', array('de_DE'))//Config::item('locale.available_langs'))
+			->set('avail_langs', array('de_DE'))//Kohana::config('locale.available_langs'))
 			->set('accept_lang', $this->lang);
 	}
 
@@ -81,4 +86,53 @@ class Installation_Controller extends Hanami_Frontend_Controller {
 
 
 
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	public function _render()
+	{
+		$lang = Kohana::config('locale.language');
+		$this->template
+			->set_global('lang', substr($lang[0], 0, 2))
+			//->set('doctype', View::factory('doctypes/'.(($this->xhtml) ? 'xhtml' : 'html')))
+			//->set('content_type', (($this->xhtml) ? 'application/xhtml+xml' : 'text/html'))
+			//->set('charset', 'utf-8')
+			//->set('page_id', $this->page_id)
+			->set('page_title', $this->page->title());
+			//->set('title', !empty($this->title) ? $this->title : end($this->page_title))
+			//->set('site_name', $this->config['site_name'])
+			//	->set('author', $this->config['author'])
+			//	->set('description', $this->config['site_description'])
+			//	->set('keywords', $this->config['keywords'])
+			//->set('header', $this->header
+			//	->set('site_name', $this->config['site_name'])
+			//	->set('site_slogan', $this->config['page_title']))
+				// ->set('breadcrumb', View::factory('breadcrumb')->set('crumbs', html::breadcrumb()))
+			// ->set('footer', $this->footer);
+
+		// Convert xhtml to html
+		Event::add('system.display', array('html', 'convert'));
+
+		// Display the loaded template
+		parent::_render();
+	}
+
+} // End Installation Controller
