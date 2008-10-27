3107c7dd396cf70f604eae6c00f201dba7399066
dpommer <dpommer@c028e98f-4550-0410-83cb-ff94a89eda61>
2008-10-27 06:50:00 +0000

    Updating several config/routes.php

--- branches/dyron/modules/blog/config/routes.php
+++ branches/dyron/modules/blog/config/routes.php
@@ -1,16 +1,4 @@
-<?php /*defined('SYSPATH') or die('No direct access allowed.');
-
-$config = array
-(
-	// Default route to use when no URI segments are available.
-	//'_default'                      => 'blog/articles',
-	
-	'blog/(?:19|20)[0-9]{2}/(?:0[1-9]|1[012])/(?:0[1-9]|[12][0-9]|3[01])/(.+)' => 'blog/article/$1',
-	'blog'                           => 'blog/articles',
-	// 'blog/archive'                  => 'blog/archive',
-);
-
-<?php*/
+<?php
 /**
  * @package  Core
  *
@@ -19,45 +7,47 @@ $config = array
  *     $config['default'] = array
  *     (
  *         // Default routing
- *         :controller/:method/:id',
+ *         'route' => array
+ *         (
+ *             'uri' => :controller/:method/:id',
+ *         ),
  *         
  *         // Defaults for route keys
  *         'controller' => 'welcome',
  *         'method' => 'index',
  *     );
  *
- * The converted regex for this route is:
- *
- *     (?:([^/]++)(?:/([^/]++)(?:/([^/]++))?)?)?
- *
  * To define a specific pattern for a key, you can use the special "regex" key:
  *
  *     $config['default'] = array
  *     (
  *         // Limit the controller to letters and underscores
- *         'regex' => array('controller' => '[a-z_]+'),
+ *         'route' => array
+ *         (
+ *             'regex' => array('controller' => '[a-z_]+')
+ *         ),
  *     );
  *
  * To add a prefix to any key, you can use the special "prefix" key:
  *
  *     $config['admin'] = array
  *     (
- *         'admin/:controller/:method/:id',
- *         
- *         // Will change all controllers to admin_:controller
- *         'prefix' => array('controller' => 'admin_'),
+ *         'route' => array
+ *         (
+ *             'prefix' => array('controller' => 'admin_'),
+ *         ),
  *     );
  *
  */
 $config['default'] = array
 (
-	// Default routing
-	':controller/:method/:id',
+	'route' => array
+	(
+		// Default routing
+		'uri' => ':controller/:method/:id',
+	),
 
 	// Defaults for route keys
 	'controller' => 'blog',
 	'method' => 'index',
-
-    // Limit the controller to letters and underscores
-    'regex' => array('method' => '[a-z_]+'),
 );
