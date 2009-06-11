5509eb32145d91604168a6ea6ac1b19373912862
dpommer <dpommer@c028e98f-4550-0410-83cb-ff94a89eda61>
2009-06-11 07:31:13 +0000

    Adding basic theme implementation

---
+++ installation/views/template.php
@@ -0,0 +1,17 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<html>
+  <head>
+    <meta name="content-type" content="text/html;charset=utf-8">
+
+    <link type="text/css" rel="stylesheet" media="screen" href="/styles/screen.css" title="no title" charset="utf-8">
+
+    <title><?php echo $page_title ?></title>
+  </head>
+
+  <body class="installation">
+    <div id="page">
+<?php echo $content ?>
+
+    </div>
+  </body>
+</html>
