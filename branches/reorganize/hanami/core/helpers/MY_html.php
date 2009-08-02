94544fb1280d802bec260b597d366f0b0ff34e55
dpommer <dpommer@c028e98f-4550-0410-83cb-ff94a89eda61>
2009-08-02 07:59:31 +0000

    SVN:keywords test

--- branches/reorganize/hanami/core/helpers/MY_html.php
+++ branches/reorganize/hanami/core/helpers/MY_html.php
@@ -1,10 +1,15 @@
-<?php 
-
+<?php defined('SYSPATH') OR die('No direct access allowed.');
 /**
+ * HTML helper extension.
  *
+ * $Id$
+ *
+ * @package    Hanami
+ * @author     Hanami Team ($Author$)
+ * @copyright  §copyright$
+ * @license    $Licence$
  */
-class html extends html_Core 
-{
+class html extends html_Core {
 
 	/**
 	 * Converts the XHTML output to HTML
