6946383c697b8653626e677a977408bea965f1ba
dpommer <dpommer@c028e98f-4550-0410-83cb-ff94a89eda61>
2009-11-19 09:30:20 +0000

    * Moving install to hanami/install

---
+++ hanami/install/i18n/de_DE/install.php
@@ -0,0 +1,12 @@
+<?php 
+
+$lang = array
+(
+	'available_languages' => 'zur Verfügung stehende Sprachen',
+	'choose_language'     => 'Bitte wähle eine Sprache aus:',
+	'confirm'             => 'Bestätigen',
+	'installation'        => 'Installation',
+	'license'             => 'Lizenz',
+	'next'                => 'Weiter',
+	'welcome'             => 'Willkommen',
+);
