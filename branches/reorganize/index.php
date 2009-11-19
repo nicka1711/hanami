3169fb8c6ac7a93d8b61de009aaa34709426ad08
dpommer <dpommer@c028e98f-4550-0410-83cb-ff94a89eda61>
2009-11-19 14:04:59 +0000

    Setting svn:keywords

--- branches/reorganize/index.php
+++ branches/reorganize/index.php
@@ -71,7 +71,7 @@ define('EXT', '.php');
 //
 // DO NOT EDIT BELOW THIS LINE, UNLESS YOU FULLY UNDERSTAND THE IMPLICATIONS.
 // ----------------------------------------------------------------------------
-// $Id: index.php 3915 2009-01-20 20:52:20Z zombor $
+// $Id$
 //
 
 $kohana_pathinfo = pathinfo(__FILE__);
